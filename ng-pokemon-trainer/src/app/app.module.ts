dba0c440803ebeff33f96f1899e14a6bf300cbe5
Steffen Tømmerås <steffen.tommeras@gmail.com>
2023-01-30 10:59:07 +0100

    feat: create page components, setup routes

--- ng-pokemon-trainer/src/app/app.module.ts
+++ ng-pokemon-trainer/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    CataloguePage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
