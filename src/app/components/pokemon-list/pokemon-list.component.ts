cce20cae0bbc5e88fbbc2a78f1d80c8695f30237
Erik Alstad <erik.ornsrud.alstad@no.experis.com>
2023-01-31 09:17:04 +0100

    feat: Pokemon list component input

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit{
+
+  @Input() pokemonArr: Pokemon[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
