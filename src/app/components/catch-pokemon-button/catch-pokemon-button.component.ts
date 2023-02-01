a46a7bb705837a4f172de39ce91f69a3a56c03ae
Steffen Tømmerås <steffen.tommeras@gmail.com>
2023-02-01 12:59:21 +0100

    feat: conditional rendering of the button

--- src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
+++ src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
@@ -1,36 +1,42 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input } from '@angular/core';
-import { User } from 'src/app/models/user.model';
-import { CatchPokemonService } from 'src/app/services/catch-pokemon.service';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, Input, OnInit } from "@angular/core";
+import { User } from "src/app/models/user.model";
+import { CatchPokemonService } from "src/app/services/catch-pokemon.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
-  selector: 'app-catch-pokemon-button',
-  templateUrl: './catch-pokemon-button.component.html',
-  styleUrls: ['./catch-pokemon-button.component.css']
+    selector: "app-catch-pokemon-button",
+    templateUrl: "./catch-pokemon-button.component.html",
+    styleUrls: ["./catch-pokemon-button.component.css"],
 })
+export class CatchPokemonButtonComponent implements OnInit {
+    public isCaught: boolean = false;
+    @Input() pokemonName: string = "";
 
-export class CatchPokemonButtonComponent {
-  // Dewald bruker ID -- Discuss
-  @Input() pokemonName: string = "";
+    get loading(): boolean {
+        return this.catchPokemonService.loading;
+    }
 
-  get loading(): boolean {
-    return this.catchPokemonService.loading;
-  }
+    constructor(
+        private userService: UserService,
+        private readonly catchPokemonService: CatchPokemonService
+    ) {}
 
-  constructor(
-    private readonly catchPokemonService: CatchPokemonService
-  ) {}
+    ngOnInit(): void {
+        this.isCaught = this.userService.inCaughtPokemon(this.pokemonName);
+    }
 
-  onCatchClick(): void {
-    alert("caught " + this.pokemonName);
-    this.catchPokemonService.addToCaughtPokemon(this.pokemonName)
-      .subscribe({
-        next: (response: User) => {
-          console.log("NEXT", response);
-        },
-        error: (error: HttpErrorResponse) => {
-          console.log("ERROR", error.message);
-        }
-      })
-  }
+    onCatchClick(): void {
+        alert("caught " + this.pokemonName);
+        this.catchPokemonService
+            .addToCaughtPokemon(this.pokemonName)
+            .subscribe({
+                next: (response: User) => {
+                    console.log("NEXT", response);
+                },
+                error: (error: HttpErrorResponse) => {
+                    console.log("ERROR", error.message);
+                },
+            });
+    }
 }
