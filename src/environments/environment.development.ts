2c6dd3732c18d3b76fd60319df1a71eb979c83c8
Steffen Tømmerås <steffen.tommeras@gmail.com>
2023-02-03 15:40:19 +0100

    fix: remove environments from gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiKey: "xq74Q7mqLP3W0nJEz0UCccjpMwNPCqFDU1K1mwWtf1tcgO9wkoVgYpieDTmAowmX",
+    apiUsers: "https://eajlst-noroff-api.up.railway.app/trainers",
+    apiPokemon: "https://pokeapi.co/api/v2/pokemon/",
+    apiSprites: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/",
+    apiSmallSprites: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/"
+}
