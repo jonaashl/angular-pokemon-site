64f5281646617bd8a9c04d59b745dd2f5f6ee885
Steffen Tømmerås <steffen.tommeras@gmail.com>
2023-02-03 16:17:48 +0100

    chore: rename login to landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./guards/auth.guard";
 import { CataloguePage } from "./pages/catalogue/catalogue.page";
-import { LoginPage } from "./pages/login/login.page";
+import { LandingPage } from "./pages/landing/landing.page";
 import { TrainerPage } from "./pages/trainer/trainer.page";
 
 const routes: Routes = [
     {
         path: "",
         pathMatch: "full",
-        component: LoginPage
+        component: LandingPage
     },
     {
-        path: "login",
-        component: LoginPage
+        path: "landing",
+        component: LandingPage
     },
     {
         path: "catalogue",
