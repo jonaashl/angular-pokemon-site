4d9ef5bb1af26cef868a7a9922f17c0bdb403d6c
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-02-03 15:14:51 +0100

    feat: added username constraints

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormControl, NgForm, Validators, FormsModule, FormGroup } from "@angular/forms";
 import { User } from "src/app/models/user.model";
 import { LoginService } from "src/app/services/login.service";
 import { UserService } from "src/app/services/user.service";
@@ -10,21 +10,27 @@ import { UserService } from "src/app/services/user.service";
     styleUrls: ["./login-form.component.css"],
 })
 export class LoginFormComponent {
-
+    
     @Output() login: EventEmitter<void> = new EventEmitter();
+    
+    usernameControl = new FormControl("", [Validators.required]);
 
     constructor(
         private readonly loginService: LoginService,
         private readonly userService: UserService,
-    ) {}
+        ) {}
 
-    public loginSubmit(loginForm: NgForm): void {
-        const { username } = loginForm.value;
-
-        this.loginService.login(username).subscribe({
-            next: (user: User) => {
-                this.userService.user = user;
-                this.login.emit();
+        public loginSubmit(loginForm: NgForm): void {
+            if(loginForm.invalid) {
+                this.usernameControl.get("username");
+                return;
+            }
+            const { username } = loginForm.value;
+        
+            this.loginService.login(username).subscribe({
+                next: (user: User) => {
+                    this.userService.user = user;
+                    this.login.emit();
             },
             error: () => {},
         });
